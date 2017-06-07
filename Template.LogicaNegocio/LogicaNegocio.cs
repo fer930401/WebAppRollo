6f59ba5005afdbe3232e438641e5f8dc13aff1ee
Fernando Garcia <fer_930401@hotmail.com>
2017-06-07 17:03:24 -0500

    LogicaNegocio
    LogicaNegocio

---
+++ Template.LogicaNegocio/LogicaNegocio.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rollos.AccesoDatos;
+
+namespace Rollos.LogicaNegocio
+{
+    public class LogicaNegocioCls
+    {
+        AccesoDatos.AccesoDatosCls datos;
+        public LogicaNegocioCls()
+        {
+            datos = new AccesoDatosCls();
+        }
+        public List<Entidades.sp_qrollosxaut_Result> consRollos(string ef_cve, string tipdoc_cve, short? sw)
+        {
+            return datos.consRollos(ef_cve, tipdoc_cve, sw);
+        }
+        public List<Entidades.sp_WebAppRollos_Result> autorizarRollos(string articulo, string ele_cve, string calidad, decimal ele_exis_um, string uni_uso, int? batch, string cliente, int? si, int? no, int? opc)
+        {
+            return datos.autorizarRollos(articulo, ele_cve, calidad, ele_exis_um, uni_uso, batch, cliente, si, no, opc);
+        }
+    }
+}
---
+++ Template.LogicaNegocio/variables.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rollos.LogicaNegocio
+{
+    public class variables
+    {
+        static string pedido;
+        public static string Pedido
+        {
+            get { return variables.pedido; }
+            set { variables.pedido = value; }
+        }
+        static string dispo;
+        public static string Dispos
+        {
+            get { return variables.dispo; }
+            set { variables.dispo = value; }
+        }
+        static string cliente;
+        public static string Cliente
+        {
+            get { return variables.cliente; }
+            set { variables.cliente = value; }
+        }
+        static string articulo;
+        public static string Articulo
+        {
+            get { return variables.articulo; }
+            set { variables.articulo = value; }
+        }
+    }
+}
